123cee9505425a7e6bef4101a68c75b9d73c17a7
AleMichelle <lizard.macl18@gmail.com>
2023-03-14 09:57:03 -0600

    tb

--- Menu.php
+++ Menu.php
@@ -8,7 +8,7 @@ $usuario = $_SESSION['usuario'];
 <html lang="en">
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.php">Home</a>
+            <a class="navbar-brand ps-3" href="index.php">INICIO</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
