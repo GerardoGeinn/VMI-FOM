e14344e75782e40123a1d855f960839cb76b914b
AleMichelle <lizard.macl18@gmail.com>
2023-03-15 17:41:20 -0600

    Comercial

--- comercialt.php
+++ comercialt.php
@@ -1,4 +1,33 @@
 
+<?php
+
+$resp="";
+
+    $resp = "SELECT TOP 100
+    DOC.CSERIEDOCUMENTO AS SERIE, CONVERT(INT,DOC.CFOLIO) AS FOLIO, ISNULL(AG.CNOMBREAGENTE,'')AS AGENTE, DOC.CTOTALUNIDADES AS CANTIDAD, 
+    PRO.CCODIGOPRODUCTO AS CODIGO,PRO.CNOMBREPRODUCTO AS DESCRIPCION, ALM.CNOMBREALMACEN AS ALMACEN, FORMAT(MOV.CPRECIO,'C','en-us') AS PRECIOP,  
+    CONVERT(VARCHAR,DOC.CFECHA,103) AS FECHA, DOC.CTOTAL, FORMAT(DOC.CTOTAL, 'C','en-us') AS MONTOT
+    FROM admProductos PRO
+    LEFT OUTER JOIN dbo.admMovimientos MOV ON PRO.CIDPRODUCTO= MOV.CIDPRODUCTO 
+    LEFT OUTER JOIN dbo.admDocumentos DOC ON MOV.CIDDOCUMENTO = DOC.CIDDOCUMENTO 
+    LEFT OUTER JOIN dbo.admUnidadesMedidaPeso UMP ON MOV.CIDUNIDAD =UMP.CIDUNIDAD
+    LEFT OUTER JOIN dbo.admDocumentosModelo DM ON MOV.CIDDOCUMENTODE = DM.CIDDOCUMENTODE
+    LEFT OUTER JOIN dbo.admAgentes AG ON DOC.CIDAGENTE = AG.CIDAGENTE
+    LEFT OUTER JOIN dbo.admAlmacenes ALM ON MOV.CIDALMACEN = ALM.CIDALMACEN
+    WHERE DOC.CCANCELADO = 0 AND  PRO.CTIPOPRODUCTO = 1 AND PRO.CSTATUSPRODUCTO=1 AND DM.CIDDOCUMENTODE= 2 AND DOC.CFOLIO<>0";
+
+$respuesta = 0;
+
+
+    include "Procesos/login/conexioncom.php";
+    $conn = new conexionFom();
+    $conexion = $conn->conectarFom();
+    $sql = $resp;
+    $res = $conexion->query($sql);
+    $respuesta = $res->fetchAll(PDO::FETCH_OBJ); 
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -8,12 +37,29 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title> Comercial-VMI</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <!--DT--> 
+        <!-- <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" /> -->
+        <!-- <link href= "https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"/> -->
+        <link rel= "https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css"/>
         <!--Boots-->   
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    
+    <style>
+        thead{
+            background-color:#212529;   
+            color:white;
+            text-align: center;
+            font-size: .70rem;
+        }
+        tbody{
+            text-align: center;
+            font-size: .70rem;
+        }
+    </style>
     </head>
+
     <body class="sb-nav-fixed">
     <?php
      include "Menu.php";
@@ -26,19 +72,54 @@
                             <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
                             <li class="breadcrumb-item active">Comercial</li>
                         </ol>
-                        <div class="card mb-4"></div>
+                     
                         
                             <!--empieza tabla-->
-                            <div class="card mb-4">
+                            <div class="container mb-4">
                                 <div class="card-header">
                                     <i class="fas fa-table me-1"></i>
-                                    Información Comercial
-                                    <hr>
-                                  <!-- <div id="tabla"> -->
-                                  <?php
-                                    include "tablacom.php";
-                                    ?>
-                                 </div>  
+                                   INFORMACIÓN COMERCIAL
+                                    
+                                    <div class="row">
+                                    <div class="col-lg-12 col-sm-12">            
+                                    <table id="tablaReport" class="table table-hover table-bordered" cellspacing="0" width="100%">
+                                    <thead>         
+                                        
+                                        <th>SERIE</th>
+                                        <th>FOLIO</th>
+                                        <th>AGENTE</th>
+                                        <th>CANTIDAD</th>
+                                    <!-- <th>EMPRESA</th>-->
+                                        <th>CÓDIGO DE PRODUCTO</th>
+                                        <th>DESCRIPCION DEL PRODUCTO</th>
+                                        <th>ALMACEN DE PRODUCTO</th>
+                                        <th>FECHA</th>
+                                        <th>PRECIO PRODUCTO</th>
+                                        <th>MONTO TOTAL</th>
+                                                
+                                    </thead>
+                                    <tbody> 
+                                    <?php foreach ($respuesta as $mostrar): ?>
+                                    <tr>
+                                    <td><?php echo $mostrar->SERIE ?></td>
+                                    <td><?php echo $mostrar->FOLIO ?></td>
+                                    <td><?php echo $mostrar->AGENTE ?></td>
+                                    <td><?php echo $mostrar->CANTIDAD ?></td>
+                                    <td><?php echo $mostrar->CODIGO ?></td>
+                                    <td><?php echo $mostrar->DESCRIPCION ?></td>
+                                    <td><?php echo $mostrar->ALMACEN ?></td>
+                                    <td><?php echo $mostrar->FECHA ?></td>
+                                    <td><?php echo $mostrar->PRECIOP ?></td>
+                                    <td><?php echo $mostrar->MONTOT ?></td>
+                                    </tr>
+                                    <?php endforeach; ?>
+                                    </tbody>
+
+                                    </table>                        
+                                
+                                    </div>  
+                                    </div>  
+                                </div>  
                             </div><!--cierrra tabla-->
                     </div>
                 </main>
@@ -47,17 +128,26 @@
                 ?>
            </div>             
         <!--JQ--> 
-        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
         <script src= https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js></script>
         <script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"> </script> 
         <script src="js/scripts.js"></script>
         <!--Boots-->    
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <!--DT--> 
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-        <script src= https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js></script>
+        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>    
+        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>       
         <!--DB--> 
-        <!-- <script src="tcomercial.js"></script> -->
+         <script>
+            $(document).ready(function () {
+            $('#tablaReport').DataTable({
+                responsive: true,
+                language: {
+            url: "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+            }
+            });
+            });
+         </script>
     </body>
     
 </html>
--- tcomercial.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('#tablaReport').DataTable(datatablesSimple).innerHTML ;
-     });
- 
- 
- 
