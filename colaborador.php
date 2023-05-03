3a77dde62851ef59581be58e3ee5819b8882db0f
AleMichelle <lizard.macl18@gmail.com>
2023-05-03 16:10:48 -0600

    front

--- colaborador.php
+++ colaborador.php
@@ -50,22 +50,22 @@ $resOrdenes = $conexionVMI->query($sqlOrdenes);
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid px-4">
-                <h1 class="mt-4">Tables</h1>
+                <h1 class="mt-4">Pedidos</h1>
                 <ol class="breadcrumb mb-4">
                     <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
-                    <li class="breadcrumb-item active">Tables</li>
+                    <li class="breadcrumb-item active">Pedidos</li>
                 </ol>
-                <div class="card mb-4">
+                <!-- <div class="card mb-4">
                     <div class="card-body">
                         DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the
                         <a target="_blank" href="https://datatables.net/">official DataTables documentation</a>
                         .
                     </div>
-                </div>
+                </div> -->
                 <div class="card mb-4">
                     <div class="card-header">
                         <i class="fas fa-table me-1"></i>
-                        DataTable Examples
+                        PEDIDOS
                     </div>
                 </div>
 
